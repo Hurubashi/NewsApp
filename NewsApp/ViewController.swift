647a65ec8b272b5c47cf9a9f27307d6bae8325c6
Max Rybak <hurubashi@gmail.com>
2019-02-13 12:19:59 +0200

    Initial Commit

---
+++ NewsApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  NewsApp
+//
+//  Created by Max Rybak on 2/13/19.
+//  Copyright © 2019 Max Rybak. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
