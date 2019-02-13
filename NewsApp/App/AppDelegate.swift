ab7e02a3ec159844c6d1b40d0b0e8567eb9d4143
Max Rybak <hurubashi@gmail.com>
2019-02-13 12:29:48 +0200

    Creating structure

---
+++ NewsApp/App/AppDelegate.swift
@@ -0,0 +1,43 @@
+//
+//  AppDelegate.swift
+//  NewsApp
+//
+//  Created by Max Rybak on 2/13/19.
+//  Copyright © 2019 Max Rybak. All rights reserved.
+//
+
+import UIKit
+import CoreData
+
+@UIApplicationMain
+class AppDelegate: UIResponder, UIApplicationDelegate {
+
+    var window: UIWindow?
+
+    // MARK: - Core Data stack
+
+    lazy var persistentContainer: NSPersistentContainer = {
+
+        let container = NSPersistentContainer(name: "NewsApp")
+        container.loadPersistentStores(completionHandler: { (storeDescription, error) in
+            if let error = error as NSError? {
+                fatalError("Unresolved error \(error), \(error.userInfo)")
+            }
+        })
+        return container
+    }()
+
+    func saveContext () {
+        let context = persistentContainer.viewContext
+        if context.hasChanges {
+            do {
+                try context.save()
+            } catch {
+                let nserror = error as NSError
+                fatalError("Unresolved error \(nserror), \(nserror.userInfo)")
+            }
+        }
+    }
+
+}
+
---
+++ NewsApp/ViewControllers/NewsList/NewsCell.swift
@@ -0,0 +1,24 @@
+//
+//  NewsCell.swift
+//  NewsApp
+//
+//  Created by Max Rybak on 2/13/19.
+//  Copyright © 2019 Max Rybak. All rights reserved.
+//
+
+import UIKit
+
+class NewsCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+}
